fd196387b795aba7a7917d52c85a6f00d2b63833
lucifer63 <lazalu68@gmail.com>
2017-06-24 21:26:38 +0300

    adjusted calc to use postMessage

--- examples/calc/js/main.js
+++ examples/calc/js/main.js
@@ -1,15 +1,17 @@
+var caller_url = 'https://lucifer63.github.io/examples/calc/caller.html';
+
 document.addEventListener("DOMContentLoaded", initCalc);
 
 function initCalc() {
 	var calc = new Calculator( document.getElementById('calculator') );
 	try {
-		opener.update({ 
+		opener.postMessage({ 
 			caller: 'calc',
 			event: 'init',
 			data: {
 				instance: calc
 			}
-		});
+		}, caller_url); 
 	} catch (error) {}
 }
 
